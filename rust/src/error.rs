5be04d4e99d7b464d95cfc5838d72770c3603326
orao-dev1 <orao-dev1@users.noreply.github.com>
2022-06-28 13:38:09 +0200

    - fix build

--- rust/src/error.rs
+++ rust/src/error.rs
@@ -1,30 +1,34 @@
+#[cfg(feature = "sdk")]
 use solana_client::client_error::ClientError;
 use thiserror::Error;
 
 /// Errors associated with vrf functions.
 #[derive(Debug, Error)]
 pub enum Error {
-  /// Unable to decode data from account into `Randomness` object.
-  #[error(
-    "Unable to decode Randomness Request from account data. Reason = `{0}`"
-  )]
-  RandomnessDecodeError(String),
+    /// Unable to decode data from account into `NetworkConfiguration` object.
+    #[error("Unable to decode `NetworkConfiguration` from account data. Reason = `{0}`")]
+    ConfigurationDecodeError(String),
 
-  /// Unable to serialize Instructions in bytes form.
-  #[error("Unable to serialize Instruction")]
-  SerializationError,
+    /// Unable to decode data from account into `Randomness` object.
+    #[error("Unable to decode Randomness Request from account data. Reason = `{0}`")]
+    RandomnessDecodeError(String),
 
-  /// Solana rpc error
-  #[error("Solana RPC error")]
-  SolanaClientError(#[from] ClientError),
+    /// Unable to serialize Instructions in bytes form.
+    #[error("Unable to serialize Instruction")]
+    SerializationError,
 
-  /// Unable to find Public Key used to generate Randomness from instruction
-  #[error("Unable to find Public Key from instruction")]
-  PubkeyNotFoundError,
+    /// Solana rpc error
+    #[error("Solana RPC error")]
+    #[cfg(feature = "sdk")]
+    SolanaClientError(#[from] ClientError),
 
-  #[error("Unable to verify Randomness. Reason = `{0}`")]
-  RandomnessVerifyError(String),
+    /// Unable to find Public Key used to generate Randomness from instruction
+    #[error("Unable to find Public Key from instruction")]
+    PubkeyNotFoundError,
 
-  #[error("{0}")]
-  NotFound(String),
+    #[error("Unable to verify Randomness. Reason = `{0}`")]
+    RandomnessVerifyError(String),
+
+    #[error("{0}")]
+    NotFound(String),
 }
--- rust/src/verify.rs
+++ rust/src/verify.rs
@@ -1,63 +1,62 @@
+#![cfg(feature = "sdk")]
+
 use crate::error::Error;
 use ed25519_dalek::{PublicKey, Signature, Verifier};
 use solana_sdk::ed25519_instruction::DATA_START;
 use solana_transaction_status::{
-  EncodedConfirmedTransactionWithStatusMeta, EncodedTransaction, UiInstruction,
-  UiMessage, UiParsedInstruction,
+    EncodedConfirmedTransactionWithStatusMeta, EncodedTransaction, UiInstruction, UiMessage,
+    UiParsedInstruction,
 };
 
 /// Returns true if Transaction contains instructions to Ed25519SigVerify and OraoSolanaVrf programs.
 ///
 /// `Transaction` must be encoded in UiTransactionEncoding::JsonParsed. Otherwise it will return false.
 pub fn is_vrf_fulfilled_transaction(
-  tx: &EncodedConfirmedTransactionWithStatusMeta,
-  vrf_program_id: String,
+    tx: &EncodedConfirmedTransactionWithStatusMeta,
+    vrf_program_id: String,
 ) -> bool {
-  match &tx.transaction.transaction {
-    EncodedTransaction::Json(json) => match &json.message {
-      UiMessage::Parsed(msg) => {
-        return is_vrf_fulfilled_instructions(
-          &msg.instructions,
-          vrf_program_id,
-        );
-      }
-      _ => {}
-    },
-    _ => {}
-  }
-  false
+    match &tx.transaction.transaction {
+        EncodedTransaction::Json(json) => match &json.message {
+            UiMessage::Parsed(msg) => {
+                return is_vrf_fulfilled_instructions(&msg.instructions, vrf_program_id);
+            }
+            _ => {}
+        },
+        _ => {}
+    }
+    false
 }
 
 /// Returns true if it is Fulfill instructions.
 ///
 /// First instruction must be directed at EdSigVerify, followed by OraoVrfProgram.
 fn is_vrf_fulfilled_instructions(
-  instructions: &Vec<UiInstruction>,
-  vrf_program_id: String,
+    instructions: &Vec<UiInstruction>,
+    vrf_program_id: String,
 ) -> bool {
-  if instructions.len() != 2 {
-    return false;
-  }
+    if instructions.len() != 2 {
+        return false;
+    }
 
-  let program_ids: Vec<String> = instructions
-    .into_iter()
-    .map(|instruction| {
-      match instruction {
-        UiInstruction::Parsed(parsed) => match parsed {
-          UiParsedInstruction::PartiallyDecoded(decoded) => {
-            return decoded.program_id.clone();
-          }
-          _ => {}
-        },
-        _ => {}
-      }
-      "".to_string()
-    })
-    .collect();
+    let program_ids: Vec<String> = instructions
+        .into_iter()
+        .map(|instruction| {
+            match instruction {
+                UiInstruction::Parsed(parsed) => match parsed {
+                    UiParsedInstruction::PartiallyDecoded(decoded) => {
+                        return decoded.program_id.clone();
+                    }
+                    _ => {}
+                },
+                _ => {}
+            }
+            "".to_string()
+        })
+        .collect();
 
-  // TODO: Check instruction of vrf_program_id
-  program_ids[0] == solana_sdk::ed25519_program::id().to_string()
-    && program_ids[1] == vrf_program_id
+    // TODO: Check instruction of vrf_program_id
+    program_ids[0] == solana_sdk::ed25519_program::id().to_string()
+        && program_ids[1] == vrf_program_id
 }
 
 /// Verify randomness with `ed25519_dalek::PublicKey` used in native EdSigVerify program.
@@ -65,67 +64,65 @@ fn is_vrf_fulfilled_instructions(
 /// First, public key is extracted from transaction containing EdSigVerify instruction.
 /// It is then used to verify randomness (signature) against seed (message).
 pub fn verify_randomness_offchain(
-  tx: &EncodedConfirmedTransactionWithStatusMeta,
-  seed: &[u8],
-  randomness: &[u8],
+    tx: &EncodedConfirmedTransactionWithStatusMeta,
+    seed: &[u8],
+    randomness: &[u8],
 ) -> Result<(), Error> {
-  let public_key = extract_pubkey_from_edsigverify_tx(tx)?;
-  let signature = Signature::from_bytes(randomness).unwrap();
-  public_key
-    .verify(seed, &signature)
-    .map_err(|err| Error::RandomnessVerifyError(err.to_string()))?;
-  Ok(())
+    let public_key = extract_pubkey_from_edsigverify_tx(tx)?;
+    let signature = Signature::from_bytes(randomness).unwrap();
+    public_key
+        .verify(seed, &signature)
+        .map_err(|err| Error::RandomnessVerifyError(err.to_string()))?;
+    Ok(())
 }
 
 /// Returns `ed25519_dalek::PublicKey` used in native EdSigVerify program.
 ///
 /// Extracts public key bytes from instruction sent to EdSigVerify program and construction `ed25519_dalek::PublicKey`.
 fn extract_pubkey_from_edsigverify_tx(
-  tx: &EncodedConfirmedTransactionWithStatusMeta,
+    tx: &EncodedConfirmedTransactionWithStatusMeta,
 ) -> Result<PublicKey, Error> {
-  match &tx.transaction.transaction {
-    EncodedTransaction::Json(json) => match &json.message {
-      UiMessage::Parsed(msg) => match &msg.instructions[0] {
-        UiInstruction::Parsed(parsed) => match parsed {
-          UiParsedInstruction::PartiallyDecoded(decoded) => {
-            return Ok(extract_pubkey_from_edsigverify_instruction_data(
-              decoded.data.to_string(),
-            ));
-          }
-          _ => {}
+    match &tx.transaction.transaction {
+        EncodedTransaction::Json(json) => match &json.message {
+            UiMessage::Parsed(msg) => match &msg.instructions[0] {
+                UiInstruction::Parsed(parsed) => match parsed {
+                    UiParsedInstruction::PartiallyDecoded(decoded) => {
+                        return Ok(extract_pubkey_from_edsigverify_instruction_data(
+                            decoded.data.to_string(),
+                        ));
+                    }
+                    _ => {}
+                },
+                _ => {}
+            },
+            _ => {}
         },
         _ => {}
-      },
-      _ => {}
-    },
-    _ => {}
-  }
-  return Err(Error::PubkeyNotFoundError);
+    }
+    return Err(Error::PubkeyNotFoundError);
 }
 
 /// See `extract_pubkey_from_edsigverify_tx`
 fn extract_pubkey_from_edsigverify_instruction_data(data: String) -> PublicKey {
-  let bytes = bs58::decode(data).into_vec().unwrap();
-  let public_key_offset = DATA_START;
-  PublicKey::from_bytes(&bytes[public_key_offset..public_key_offset + 32])
-    .unwrap()
+    let bytes = bs58::decode(data).into_vec().unwrap();
+    let public_key_offset = DATA_START;
+    PublicKey::from_bytes(&bytes[public_key_offset..public_key_offset + 32]).unwrap()
 }
 
 #[cfg(test)]
 mod tests {
-  use super::*;
-  use ed25519_dalek::PublicKey;
-  use solana_transaction_status::{
-    parse_accounts::ParsedAccount, EncodedConfirmedTransactionWithStatusMeta,
-    EncodedTransaction, EncodedTransactionWithStatusMeta, UiInstruction,
-    UiMessage, UiParsedInstruction, UiParsedMessage,
-    UiPartiallyDecodedInstruction, UiTransaction,
-  };
+    use super::*;
+    use ed25519_dalek::PublicKey;
+    use solana_transaction_status::{
+        parse_accounts::ParsedAccount, EncodedConfirmedTransactionWithStatusMeta,
+        EncodedTransaction, EncodedTransactionWithStatusMeta, UiInstruction, UiMessage,
+        UiParsedInstruction, UiParsedMessage, UiPartiallyDecodedInstruction, UiTransaction,
+    };
 
-  const program_id: &str = "VRFUm3dhiqtyW6nj8XghcPLJbCXg9Hj85iABpxwq1Xz";
+    const program_id: &str = "VRFUm3dhiqtyW6nj8XghcPLJbCXg9Hj85iABpxwq1Xz";
 
-  fn new_verify_tx() -> EncodedConfirmedTransactionWithStatusMeta {
-    EncodedConfirmedTransactionWithStatusMeta {
+    fn new_verify_tx() -> EncodedConfirmedTransactionWithStatusMeta {
+        EncodedConfirmedTransactionWithStatusMeta {
 			slot: 135220546,
 			transaction: EncodedTransactionWithStatusMeta {
 				transaction: EncodedTransaction::Json(UiTransaction {
@@ -165,10 +162,10 @@ mod tests {
 			},
 			block_time: Some(1652942324),
 		}
-  }
+    }
 
-  fn new_request_tx() -> EncodedConfirmedTransactionWithStatusMeta {
-    EncodedConfirmedTransactionWithStatusMeta {
+    fn new_request_tx() -> EncodedConfirmedTransactionWithStatusMeta {
+        EncodedConfirmedTransactionWithStatusMeta {
 			slot: 135220546,
 			transaction: EncodedTransactionWithStatusMeta {
 				transaction: EncodedTransaction::Json(UiTransaction {
@@ -201,58 +198,51 @@ mod tests {
 			},
 			block_time: Some(1652942324),
 		}
-  }
+    }
 
-  #[test]
-  fn test_is_vrf_fulfilled_transaction() {
-    let verify_tx = new_verify_tx();
-    assert_eq!(
-      is_vrf_fulfilled_transaction(&verify_tx, program_id.to_string()),
-      true
-    );
-    let request_tx = new_request_tx();
-    assert_eq!(
-      is_vrf_fulfilled_transaction(&request_tx, program_id.to_string()),
-      false
-    );
-  }
+    #[test]
+    fn test_is_vrf_fulfilled_transaction() {
+        let verify_tx = new_verify_tx();
+        assert_eq!(
+            is_vrf_fulfilled_transaction(&verify_tx, program_id.to_string()),
+            true
+        );
+        let request_tx = new_request_tx();
+        assert_eq!(
+            is_vrf_fulfilled_transaction(&request_tx, program_id.to_string()),
+            false
+        );
+    }
 
-  #[test]
-  fn test_extract_pubkey() {
-    let data = "4Em4VK9kSwLdJbfKHSpHeP2NLFMnJpKVtwPiVeqKLJa8tmATUEUYTbk1PMWMBi8nKfyN3p9cYc4WWkCVy2hZegpvgvXHapPxX8JWUhTUaGZUEhx8PNHNwWJHpD3j9S82kYj6K3He2BJUEtejUHX241v9ApmiVnUpxNbN3J3dgYuvJHMqwF4wF21jcrAnfse571wK".to_string();
-    let expected_pubkey = PublicKey::from_bytes(
-      vec![
-        22, 9, 56, 134, 48, 255, 91, 50, 91, 100, 234, 229, 1, 85, 222, 13,
-        136, 140, 147, 52, 139, 118, 42, 145, 105, 28, 180, 197, 75, 12, 124,
-        228,
-      ]
-      .as_ref(),
-    )
-    .unwrap();
-    let extracted_pubkey =
-      extract_pubkey_from_edsigverify_instruction_data(data);
-    assert_eq!(expected_pubkey, extracted_pubkey);
-  }
+    #[test]
+    fn test_extract_pubkey() {
+        let data = "4Em4VK9kSwLdJbfKHSpHeP2NLFMnJpKVtwPiVeqKLJa8tmATUEUYTbk1PMWMBi8nKfyN3p9cYc4WWkCVy2hZegpvgvXHapPxX8JWUhTUaGZUEhx8PNHNwWJHpD3j9S82kYj6K3He2BJUEtejUHX241v9ApmiVnUpxNbN3J3dgYuvJHMqwF4wF21jcrAnfse571wK".to_string();
+        let expected_pubkey = PublicKey::from_bytes(
+            vec![
+                22, 9, 56, 134, 48, 255, 91, 50, 91, 100, 234, 229, 1, 85, 222, 13, 136, 140, 147,
+                52, 139, 118, 42, 145, 105, 28, 180, 197, 75, 12, 124, 228,
+            ]
+            .as_ref(),
+        )
+        .unwrap();
+        let extracted_pubkey = extract_pubkey_from_edsigverify_instruction_data(data);
+        assert_eq!(expected_pubkey, extracted_pubkey);
+    }
 
-  #[test]
-  fn test_verify() {
-    let verify_tx = new_verify_tx();
-    let seed = vec![
-      240, 76, 119, 173, 85, 132, 29, 69, 30, 159, 184, 65, 43, 241, 28, 248,
-      97, 201, 145, 10, 67, 132, 18, 9, 218, 186, 126, 72, 25, 57, 120, 30,
-    ];
-    let randomness = vec![
-      205, 235, 166, 115, 6, 150, 100, 51, 119, 192, 239, 49, 225, 84, 0, 134,
-      54, 41, 137, 152, 217, 73, 1, 238, 109, 226, 212, 225, 127, 168, 205,
-      149, 202, 44, 29, 236, 199, 104, 136, 54, 18, 251, 206, 202, 117, 28, 64,
-      145, 220, 35, 103, 52, 255, 153, 28, 7, 159, 95, 10, 107, 164, 145, 234,
-      10,
-    ];
-    let res = verify_randomness_offchain(
-      &verify_tx,
-      seed.as_ref(),
-      randomness.as_ref(),
-    );
-    assert_eq!(res.is_ok(), true);
-  }
+    #[test]
+    fn test_verify() {
+        let verify_tx = new_verify_tx();
+        let seed = vec![
+            240, 76, 119, 173, 85, 132, 29, 69, 30, 159, 184, 65, 43, 241, 28, 248, 97, 201, 145,
+            10, 67, 132, 18, 9, 218, 186, 126, 72, 25, 57, 120, 30,
+        ];
+        let randomness = vec![
+            205, 235, 166, 115, 6, 150, 100, 51, 119, 192, 239, 49, 225, 84, 0, 134, 54, 41, 137,
+            152, 217, 73, 1, 238, 109, 226, 212, 225, 127, 168, 205, 149, 202, 44, 29, 236, 199,
+            104, 136, 54, 18, 251, 206, 202, 117, 28, 64, 145, 220, 35, 103, 52, 255, 153, 28, 7,
+            159, 95, 10, 107, 164, 145, 234, 10,
+        ];
+        let res = verify_randomness_offchain(&verify_tx, seed.as_ref(), randomness.as_ref());
+        assert_eq!(res.is_ok(), true);
+    }
 }
